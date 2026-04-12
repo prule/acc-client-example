c47cf6d89033f5f070e3fd447f7d690a80eeebe0
paulrule <paulrule1@gmail.com>
2026-04-12 19:58:42 +1000

    publish source and docs

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -1,29 +1,41 @@
-// The code in this file is a convention plugin - a Gradle mechanism for sharing reusable build logic.
-// `buildSrc` is a Gradle-recognized directory and every plugin there will be easily available in the rest of the build.
 package buildsrc.convention
 
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin in JVM projects.
-    kotlin("jvm")
+  kotlin("jvm")
+  `maven-publish`
+  id("org.jetbrains.dokka")
 }
 
-kotlin {
-    // Use a specific Java version to make it easier to work in different environments.
-    jvmToolchain(25)
+configure<KotlinJvmProjectExtension> { jvmToolchain(21) }
+
+configure<JavaPluginExtension> { withSourcesJar() }
+
+val dokkaJar by
+    tasks.registering(Jar::class) {
+      archiveClassifier.set("javadoc")
+      from(tasks.named("dokkaGeneratePublicationHtml"))
+    }
+
+configure<PublishingExtension> {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+      artifact(dokkaJar)
+    }
+  }
 }
 
 tasks.withType<Test>().configureEach {
-    // Configure all test Gradle tasks to use JUnitPlatform.
-    useJUnitPlatform()
-
-    // Log information about all test results, not only the failed ones.
-    testLogging {
-        events(
-            TestLogEvent.FAILED,
-            TestLogEvent.PASSED,
-            TestLogEvent.SKIPPED,
-        )
-    }
+  useJUnitPlatform()
+  testLogging {
+    events(
+        TestLogEvent.FAILED,
+        TestLogEvent.PASSED,
+        TestLogEvent.SKIPPED,
+    )
+  }
 }
